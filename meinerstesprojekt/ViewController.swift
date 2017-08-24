e4a076aba5b58e3ce39e94721540fb622623dec8
Thomas Kaltschmidt <tkaltschmidt@tks-imac-heise-2.local>
2017-08-24 16:13:52 +0200

    Initial Commit

---
+++ meinerstesprojekt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  meinerstesprojekt
+//
+//  Created by tkaltschmidt on 24.08.17.
+//  Copyright © 2017 tkaltschmidt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
